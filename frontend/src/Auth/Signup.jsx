a93cf3388f4b390053f5237feddbc389cdb3f103
Shahrukh Haider Nagnur <50351366+shahrukhnagnur@users.noreply.github.com>
2025-03-14 14:18:48 +0530

    Update Signup.jsx

--- frontend/src/Auth/Signup.jsx
+++ frontend/src/Auth/Signup.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./auth.css"; // Import the CSS for styling
+import "./auth.css"; 
 
 const Signup = () => {
   const [formData, setFormData] = useState({
