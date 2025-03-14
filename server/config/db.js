befc9d426ffbaaa783508d4884a38e94f28f04e2
Shahrukh Haider Nagnur <50351366+shahrukhnagnur@users.noreply.github.com>
2025-03-14 14:16:48 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-require("dotenv").config(); // Load environment variables from .env file
+require("dotenv").config();
 
 const connectDB = async () => {
   try {
